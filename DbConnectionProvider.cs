009b51d21d3349023f11ea92ce4df43cf681c8f5
Nelson Marro <nelsonmarro99@gmail.com>
2023-06-08 08:30:56 -0500

    Add project files.

---
+++ DbConnectionProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CRUD
+{
+  public class DbConnectionProvider
+  {
+    public static SqlConnection GetConnection =
+      new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+    
+    private DbConnectionProvider() { }
+  }
+}
